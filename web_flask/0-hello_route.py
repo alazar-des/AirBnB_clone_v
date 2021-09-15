873c2823ccb468921fef89074580553b4cff8f4f
alazar-des <alazar.des@gmail.com>
2021-09-15 16:11:05 +0300

    flask route

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Start a web application.
+"""
+from flask import Flask
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello():
+    """return hello hbnb.
+    """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb')
+def hbnb():
+    """Return hbnb.
+    """
+    return "HBNB"
